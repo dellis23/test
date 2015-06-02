5e04836155234fcb2e56dcd8375cfa6a46fa6941
Dan Ellis <dan.ellis@saucelabs.com>
2015-06-01 20:40:26 -0700

    Add failing test.

--- test.py
+++ test.py
@@ -17,6 +17,9 @@ class TestStringMethods(unittest.TestCase):
       with self.assertRaises(TypeError):
           s.split(2)
 
+    def test_truth(self):
+        self.assertEqual(True, False)
+
 
 if __name__ == '__main__':
     unittest.main()
